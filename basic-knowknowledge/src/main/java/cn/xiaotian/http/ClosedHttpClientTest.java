5ec500364dec81e642b2259dca3deb34f8da5664
yzxmf <18345969703@163.com>
2024-04-30 23:09:54 +0800

    20242423--一些算法

--- basic-knowknowledge/src/main/java/cn/xiaotian/http/ClosedHttpClientTest.java
+++ basic-knowknowledge/src/main/java/cn/xiaotian/http/ClosedHttpClientTest.java
@@ -1,17 +1,12 @@
 package cn.xiaotian.http;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
