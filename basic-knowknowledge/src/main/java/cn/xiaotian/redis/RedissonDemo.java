d299603a478b9699ae68a87ba65dedb2d5b2749e
田一帆 <yifan.tian@tongdun.net>
2024-05-03 23:57:58 +0800

    2024-05-03 Redissson

---
+++ basic-knowknowledge/src/main/java/cn/xiaotian/redis/RedissonDemo.java
@@ -0,0 +1,45 @@
+package cn.xiaotian.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author xiaotian
+ * @description redisson demo
+ * @date 2024-05-03 23:47
+ */
+public class RedissonDemo {
+
+    public static void main(String[] args) {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://127.0.0.1:6379")
+                .setPassword("123456")
+                .setDatabase(0);
+        RedissonClient redissonClient = Redisson.create(config);
+//获取锁对象实例
+        final String lockKey = "abc";
+        RLock rLock = redissonClient.getLock(lockKey);
+
+        try {
+            //尝试5秒内获取锁，如果获取到了，最长60秒自动释放
+            boolean res = rLock.tryLock(5L, 60L, TimeUnit.SECONDS);
+            if (res) {
+                //成功获得锁，在这里处理业务
+                System.out.println("获取锁成功");
+            }
+        } catch (Exception e) {
+            System.out.println("获取锁失败，失败原因：" + e.getMessage());
+        } finally {
+            //无论如何, 最后都要解锁
+            rLock.unlock();
+        }
+
+//关闭客户端
+        redissonClient.shutdown();
+    }
+}
